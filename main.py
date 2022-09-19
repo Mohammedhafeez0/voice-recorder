1982bda9fe00c0149179fb7f9654bdf31c44278a
Mohammedhafeez0 <93590215+Mohammedhafeez0@users.noreply.github.com>
2022-09-19 22:44:29 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#import all requird lib
+
+import sounddevice as sound
+from scipy.io.wavfile import write
+import wavio as wv
+
+
+freq=44100
+
+#duration given in sec 
+duration = 10
+recording=sound.rec(int(duration*freq),
+                     samplerate=freq,channels=2)
+sound.wait()
+write("recording.wav",freq,recording)
